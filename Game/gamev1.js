322af2f45fedb98c0d0f44ef3d1f5af28d4174da
Poddyman0 <127142059+Poddyman0@users.noreply.github.com>
2023-03-23 20:22:35 +0000

    Update gamev1.js

--- Game/gamev1.js
+++ Game/gamev1.js
@@ -1,49 +1,52 @@
-count = 1;
-var Decisions = function() {
-    if (count === 1) {
-        playerChoice = prompt("Do you choose rock, paper or scissors?");
-    } else {
-        playerChoice = prompt("It's a tie. Please make your choice again! Do you choose rock, paper or scissors?");
-    }
-    computerChoice = Math.ceil(Math.random() *3);
-        if (computerChoice < 1) {
-        computerChoice = "rock";
-    } else if(1 <= computerChoice <= 2) {
-        computerChoice = "paper";
-    } else {
-        computerChoice = "scissors";
-    }
-} 
-Decisions();
-function compare(playerChoice, computerChoice){
-    if (playerChoice === computerChoice) {
-        count = count + 1
-        console.log("The result is a tie!");
-        Decisions();
-        return compare(playerChoice, computerChoice);
-      }
-      if (playerChoice === "rock") {
-        if (computerChoice === "scissors") {
-          return "Player Wins";
-        } else {
-          return "Computer Wins";
-        }
-      }
-      if (playerChoice === "paper") {
-        if (computerChoice === "scissors") {
-          return "Computer Wins";
-        } else {
-          return "Player Wins";
-        }
-      }
-      if (playerChoice === "scissors") {
-        if (computerChoice === "rock") {
-          return "Computer Wins";
-        } else {
-                  return "Player Wins";
-        }
-      }
+let count = 1;
+let playerChoice, computerChoice;
+
+function Decisions() {
+  if (count === 1) {
+    playerChoice = prompt("Do you choose rock, paper or scissors?");
+  } else {
+    playerChoice = prompt("It's a tie. Please make your choice again! Do you choose rock, paper or scissors?");
   }
-  console.log(compare(playerChoice, computerChoice));
-  console.log("Wow, you played " + count + " games!");
-      
+  computerChoice = Math.ceil(Math.random() * 3);
+  if (computerChoice === 1) {
+    computerChoice = "rock";
+  } else if (computerChoice === 2) {
+    computerChoice = "paper";
+  } else {
+    computerChoice = "scissors";
+  }
+}
+
+function compare(playerChoice, computerChoice) {
+  if (playerChoice === computerChoice) {
+    count = count + 1;
+    console.log("The result is a tie!");
+    Decisions();
+    return compare(playerChoice, computerChoice);
+  }
+  if (playerChoice === "rock") {
+    if (computerChoice === "scissors") {
+      return "Player Wins";
+    } else {
+      return "Computer Wins";
+    }
+  }
+  if (playerChoice === "paper") {
+    if (computerChoice === "scissors") {
+      return "Computer Wins";
+    } else {
+      return "Player Wins";
+    }
+  }
+  if (playerChoice === "scissors") {
+    if (computerChoice === "rock") {
+      return "Computer Wins";
+    } else {
+      return "Player Wins";
+    }
+  }
+}
+
+Decisions();
+console.log(compare(playerChoice, computerChoice));
+console.log("Wow, you played " + count + " games!");
