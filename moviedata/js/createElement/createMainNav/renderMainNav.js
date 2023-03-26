6df91458a10efac758e2afa239ae4f1af875a7a7
Poddyman0 <127142059+Poddyman0@users.noreply.github.com>
2023-03-26 13:32:05 +0100

    Update renderMainNav.js

--- moviedata/js/createElement/createMainNav/renderMainNav.js
+++ moviedata/js/createElement/createMainNav/renderMainNav.js
@@ -16,8 +16,6 @@ export const renderMainNav = () => {
     containerId: 'nav__buttton_container__sort_movies_dropdown',
     imgClass: 'nav__button_icon',
     imgId: 'nav__button_icon__sort_movies',
-    imgSrc: './img/sort.svg',
-    imgAlt: 'Sort Movies',
     pClass: 'nav__button_text hide',
     pId: 'nav__button_text__sort_moives',
     pText: 'Sort Moives',
