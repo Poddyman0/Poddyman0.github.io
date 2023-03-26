6ee35bf45a442e3842afe962d9339efc2f5ffa51
Poddyman0 <127142059+Poddyman0@users.noreply.github.com>
2023-03-26 13:29:26 +0100

    Update createHomeBgImage.js

--- moviedata/js/createElement/createHomePage/createHomeBgImage.js
+++ moviedata/js/createElement/createHomePage/createHomeBgImage.js
@@ -4,8 +4,6 @@ export const createHomeBgImage = () => {
   const backgroundImage = createElement('img', {
     class: 'home__bg_img',
     id: '  id="home__bg_img',
-    src: './img/movie_bg8.png',
-    alt: 'movie clapper board and light',
   })
 
   return backgroundImage
